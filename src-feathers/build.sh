dac2b09e0d4e9332d285ae3e6d405dd9234bf872
Michael Cole <gitlab@michaelcole.com>
2019-06-18 15:25:03 -0700

    Docker build

--- src-feathers/build.sh
+++ src-feathers/build.sh
@@ -9,20 +9,20 @@ if `git status | grep -q "nothing to commit, working tree clean"`; then
   git checkout --quiet $BRANCH || exit 1
 else
   echo "ERROR: repo has unsaved changes.  declining to build uncommitted code."
-  #exit 1
+  exit 1
 fi
 
 # Build vote SPA and commit to game repo
 (cd .. && quasar build || exit 1) || exit 1
 # Copy to src-feathers to workaround docker: COPY failed: Forbidden path outside the build context: dist/spa ()
 rm -rf public/*
-cp -rf ../dist/spa public
+cp -rf ../dist/spa/* public
 
 # build and push Docker image
 
 VERSION=$(node -p -e "require('./package.json').version")-$(git rev-parse --short HEAD)
 docker build -t $REPO:$VERSION . || exit 1
-#docker push $REPO:$VERSION || exit 1
+docker push $REPO:$VERSION || exit 1
 
 
 # Done
