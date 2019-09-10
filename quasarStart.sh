e0c68132b3cc6d7b4f7e3a0af0135f2ced49adfb
Michael Cole <gitlab@michaelcole.com>
2019-09-10 11:22:02 -0700

    Install dependencies on dev start

--- quasarStart.sh
+++ quasarStart.sh
@@ -3,11 +3,11 @@
 # Use node version in .nvmrc if present.
 [ -s "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh" && nvm install
 
+# Install quasar and Facebook's Yarn globally if not present.  If it doesn't work, just run the 2nd command.
+npm list -g @quasar/cli || npm install -g @quasar/cli yarn
+
 # Install packages using Facebook's Yarn
 [ ! -d "node_modules" ] && yarn
-
-# Install quasar globally if not present
-npm list -g @quasar/cli || npm install -g @quasar/cli  
 
 # Start dev server
 quasar dev
